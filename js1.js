b283ebf53c0062bcc4a529e10e05caa9ab2d8661
DmitryD2019 <49689964+DmitryD2019@users.noreply.github.com>
2019-05-19 22:21:04 +0300

    Add files via upload

--- js1.js
+++ js1.js
@@ -1,46 +1,83 @@
 const CONST_VALUE = 123;
 
 //Задача 1
-//Данные
+let height;
+let width;
+let balance;
+let sum;
+
 height = 25;
 width = 15;
 balance = 1000;
 
-//1 способ
-sum = balance - (height * width);
+sum = balance - (height * width * 2);
 console.log(sum);
-
-//2 способ
-sum = (height * width);
-height = 25;
-width = 15;
-balance = 1000;
-
-total = (balance - sum);
-console.log(total); 
-
-
-//Задача 4
-
-m = 8.5;
-n = 11.45;
-
-result = (10 - m < n - 10)
-? "8.5"
-: "11.45";
-console.log(result); 
 
 
 //Задача 2
-s1 = 15;
-s2 = 6;
-s3 = s1 - s2;
+let s1 = 15;
+let s2 = 6;
+let s3 = s1 - s2;
 console.log(s3);
 
 
 //Задача 3
+let num1;
+let num2;
+let num3;
+num1 = 160;
+num2 = 150;
+num3 = 140;
 
-num1 = 150;
-num2 = 170;
-num3 = 190;
-console.log(Number.MIN_VALUE);
+if (num1 < num2 && num1 < num3) {
+console.log(num1);
+} else if (num2 < num1 && num2 < num3) {
+console.log(num2);
+} else {
+console.log(num3);  
+}
+
+/*Задача 4
+let m = 8.5;
+let n = 11.45;
+let result;
+
+result = (10 - m < n - 10)
+? "8.5"
+: "11.45";
+console.log(result); */
+
+/*Задача 4
+let m = 40;
+let n = -5;
+
+if (m > 10 || 10 > n) {
+console.log(m);
+} else {
+console.log(n);
+}*/
+
+/*let m = 8;
+let n = 11;
+if (m - n > 10 && 10 > n - m) {
+console.log(n);
+} else {
+console.log(m);
+}*/
+
+/*let m = -40;
+let n = -50;
+if (m * 10 > n * 10) {
+console.log(n);
+} else {
+console.log(m);
+}*/
+
+let m = 40
+let n = -5;
+if (10 - m > 10 - n) {
+console.log(m);
+} else {
+console.log(n);
+}      
+
